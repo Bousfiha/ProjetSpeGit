4da8a3193b0c2343c5b484e4cdc1ba2c49fc1d48
messaous <samia.messaoudi@hotmail.fr>
2015-05-24 15:43:35 +0200

    Doc_cadrage
    proposition de mise en page

--- Exponentielle.r
+++ Exponentielle.r
@@ -8,11 +8,9 @@ LambdaSimu = c(1:1000)
 VarSimu = c(1:1000)
 
 for ( j in 1:NbIt) {
-
 ##Simulation de la loi
 exp = rexp(TailleEch,lambda)
 ##Estimation du paramètre lambda
-denominateur = 0
 LambdaSimu[j] = 1/mean(exp)
 }
 
@@ -22,25 +20,22 @@ VarMoy= mean(LambdaSimu^2)-LambdaSimuMoy^2
 exp = rexp(TailleEch,lambda)
 LambdaChap = 1/mean(exp)
 
+##Comparaison à une loi exponentielle
 f = pexp(exp,LambdaChap)
-##f = pexp(exp)
-Ui= f[order(f)]
 
+##Comparaison à une loi de Weibull
+##f=pweibull(exp,5,1)
+
+Ui= f[order(f)]
 ##Test de Kolmogorov Smirnov
 source("KS.r")
 D = KS(Ui,TailleEch)
 K = (D-0.2/sqrt(TailleEch))*(sqrt(TailleEch)+0.26+0.5/sqrt(TailleEch))
 
-
-
 ##Test de Cramer-von Mises
-##require(dgof)
-##y = stepfun(exp[order(exp)],c(0,Ui))
-##TestCVM = cvm.test(exp,y)
 source("CVM.r")
 W = CVM(Ui,TailleEch)
 CM = W*(1+0.16/TailleEch)
-
 
 ##Test d'Anderson Darling
 ## Calcul de la statistique d'Anderson Darling
