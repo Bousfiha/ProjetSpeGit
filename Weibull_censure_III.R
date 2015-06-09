dc06cda8ceeba789fa9abcbde08daf24f501811c
Bousfiha <amine.bousfiha93@gmail.com>
2015-06-09 09:48:30 +0200

    maj

--- Weibull_censure_III.R
+++ Weibull_censure_III.R
@@ -1,9 +1,9 @@
 ## Simulation d'un echantillion censurÃ© de type III (Multicensuré)
-simWeibIII <- function(n,beta,eta)
+simWeibIII <- function(n,beta,eta,facteur)
 {
 res = matrix(0,n,2)
 sim = rweibull(n,beta,eta)
-Tc = rweibull(n,beta,eta*0.9)
+Tc = rweibull(n,beta,eta*facteur)
 for (i in 1:n) {
   if (Tc[i]<sim[i]) {
     res[i,2] = 0
@@ -16,52 +16,81 @@ for (i in 1:n) {
 return(res)
 }
 
+
+
 beta = 3
 eta = 100
 n = 100
-RInit = n/10
-lambdaBetaRMoy = matrix(0,n,3)
-lambdaBetaRVar = matrix(0,n,3)
-
-taux_censure = R/n
-Itr = 1000
+Facteurs = seq(0.01,3.2,0.01)
+All = matrix(1,100,3) # première colonne Eta
+                     # deuxième colonne Beta
+                     # troisième colonne: nombre de sim
+k=1
 ## Calcul de la moyenne et variance des estimations de paramÃ¨tres
-  sim = simWeibIII(n,beta,eta)
-  R = sum(Ech[,2])
-  simOrdre = sim[order(sim[,2], decreasing=TRUE),]
-  ## Fontion pour estimer les paramÃ¨tres
-  f <- function(param) {
-    eta = param[1]
-    beta = param[2]
-    -(R*log(beta) - R*beta*log(eta) + (beta-1)*sum(log(simOrdre)[1:R]) - (1/(eta^beta))*(sum((simOrdre^beta)[1:R])+(n-R)*simOrdre[R]^beta)) 
-  }
-  ## Graphe de probabilite pour estimer les param initiaux de l'optim
-  graphe4 = matrix(1,R,2)
-  for (i in 1:R){
-   # if (simOrdre[i,2]==1) {
-      graphe4[i,1] = simOrdre[i]
+for (Facteur in Facteurs) {
+  for (h in 1:10) {
+    sim = simWeibIII(n,beta,eta,Facteur)
+    R = sum(sim[,2])
+    if (R<n/10)
+      break
+    simOrdre = sim[order(sim[,2], decreasing=TRUE),]
+    simObserve = simOrdre[1:R,1]
+    simObserve = simObserve[order(simObserve)]
+    ## Fontion pour estimer les paramÃ¨tres
+    f <- function(param) {
+      eta = param[1]
+      beta = param[2]
+      -(R*log(beta) - R*beta*log(eta) + (beta-1)*sum(log(simOrdre)[1:R]) - (1/(eta^beta))*(sum((simOrdre^beta)[1:R])+(n-R)*simOrdre[R]^beta)) 
+    }
+    ## Graphe de probabilite pour estimer les param initiaux de l'optim
+    graphe4 = matrix(1,R,2)
+    for (i in 1:R){
+      graphe4[i,1] = log(simObserve[i])
+      #Kaplan Meier
       for (j in 1:R) {
-          if (simOrdre[j]>simOrdre[i]) {
+          if (simObserve[j]>simObserve[i]) {
             break
           }
           graphe4[i,2] = graphe4[i,2]*(n-j)/(n-j+1)
       }
+    }
+    graphe4[,2]= log(log(1/(graphe4[,2])))
+    if (R==n) {
+      graphe4 <- graphe4[-n,]
+    }
+
+  #par(new=TRUE)
+  #plot(graphe4, col="blue", pch=16)
+
+  LM = lm(graphe4[,2]~graphe4[,1])
+  #abline(LM)
+  origine=LM$coef[1]
+  pente = LM$coef[2]
+  eta0 = exp(-origine/pente)
+  beta0 = pente
+  #Calcul de Eta et Beta
+  res <- optim(c(eta0,beta0),f)
+  EtaChapeau = res$par[1]
+  BetaChapeau = res$par[2]
+  All[R,1] = All[R,1] + EtaChapeau
+  All[R,2] = All[R,2] + BetaChapeau
+  All[R,3] = All[R,3] + 1
   }
-  graphe4[,2]= log(log(1/(1 - graphe4[,2])))
+}
 
-#par(new=TRUE)
-#plot(graphe4, type="S", ylim=c(0:1))
-
-
-
-
+All = All/All[,3]
 #Graphes d'eta
-#pdf("VarFonctionDeRII.pdf", height=5,width=10)
+pdf("EtaFonctionDeRIII.pdf", height=5,width=10)
 #par(mfrow = c(1,2))
-#plot(lambdaBetaRMoy[RInit:100,], main="Estimation de Eta en fonction de r",xlab="R", ylab="moy(Eta)", col="blue", pch=16)
-
+plot(All[10:100,1]~c(10:100), main="Estimation de Eta en fonction de r",xlab="R", ylab="Eta", col="blue", pch=16)
+abline(h = eta, col = "red", lty = "dotted",lwd=4)
+dev.off()
 #plot(lambdaBetaRVar[RInit:100,], main="Variance des estimation de Eta en fonction de r",xlab="R", ylab="Var(Eta)", col="red", pch=16)
 #Graphes de Beta
-#plot(lambdaBetaRMoy[RInit:100,-2], main="Estimation de Beta en fonction de r",xlab="R", ylab="moy(Beta)", col="blue", pch=16)
+pdf("BetaFonctionDeRIII.pdf", height=5,width=10)
+plot(All[10:100,2]~c(10:100), main="Estimation de Beta en fonction de r",xlab="R", ylab="Beta", col="red", pch=16)
+abline(h = beta, col = "blue", lty="dotted", lwd=4)
 #plot(lambdaBetaRVar[RInit:100,-2], main="Variance de Beta en fonction de r", xlab="R", ylab="Var(Beta)", col="red",  pch=16)
-#dev.off()
+dev.off()
+
+## Tests d'adéquation
